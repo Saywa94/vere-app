67446700def0a1e10c8d8f8b4d809948af03edf9
Saywa94 <saywachavez@gmail.com>
2024-03-27 17:36:03 -0400

    Minor config improvments

--- vere_app/settings.py
+++ vere_app/settings.py
@@ -22,15 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8t8*(y4#sxit_4dndzv1=5ljzmub+&%9c1eui%aka2y#ex0k#f'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'django-insecure-8t8*(y4#sxit_4dndzv1=5ljzmub+&%9c1eui%aka2y#ex0k#f'
 
 debug_environ = os.environ.get('DEBUG')
-if debug_environ == 'False':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = debug_environ == 'True'
 
-ALLOWED_HOSTS = ['https://verena-montano.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOSTS') or '127.0.0.1']
 
 
 # Application definition
@@ -122,13 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
